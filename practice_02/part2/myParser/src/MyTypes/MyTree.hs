6914df13397b6bc92c136afa0ced03be7d0ce046
Lebedevich Vlfdislav <lebedevich.vladislav@yandex.ru>
2026-04-10 01:42:17 +0300

    added instance Foldable MyTree

--- practice_02/part2/myParser/src/MyTypes/MyTree.hs
+++ practice_02/part2/myParser/src/MyTypes/MyTree.hs
@@ -8,3 +8,12 @@ instance Functor MyTree where
     fmap f (Leaf x) = Leaf (f x)
     
     fmap f (Node x left right) = Node (f x) (fmap f left) (fmap f right)
+
+instance Foldable MyTree where
+    foldr f z (Leaf x) = f x z
+    foldr f z (Node x left right) = 
+        f x (foldr f (foldr f z right) left)
+    
+    foldMap f (Leaf x) = f x
+    foldMap f (Node x left right) = 
+        f x <> foldMap f left <> foldMap f right
