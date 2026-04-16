b3ad87e0c3576553d8fd68233bd0a37ef579c590
Lebedevich Vlfdislav <lebedevich.vladislav@yandex.ru>
2026-04-16 06:14:50 +0300

    added examples for (*>), (<*), liftA2, (<$), mapped, mconcat, sconcatm stimes, fold

--- practice_02/part2/myParser/src/MyTypes/MyMaybe.hs
+++ practice_02/part2/myParser/src/MyTypes/MyMaybe.hs
@@ -8,6 +8,11 @@
 
 Добавьте в конец каждого файла многострочный комментарий: 
 для реализованного типа и определенных представителей классов типов напишите пример вызова соответствующих функций и результатов работы из ghci 
+- fold, foldMap, foldr
+- (<>), sconcat, stimes
+- mappend, mconcat
+- fmap, (<$)
+- pure, (<*>), liftA2, (*>), (<*)
 
 3. Импортируйте модули в Main.hs
 
@@ -25,7 +30,7 @@ instance Applicative MyMaybe where
     pure = MyJust
 
     MyJust f  <*> m = fmap f m
-    MyNothing <*> m = MyNothing
+    MyNothing <*> _ = MyNothing
 
 instance Semigroup a => Semigroup (MyMaybe a) where
     MyNothing <> b = b
@@ -43,12 +48,21 @@ instance Foldable MyMaybe where
     foldMap f (MyJust x) = f x
 
 {-
+ghci> :l MyTypes.MyMaybe
+ghci> :module + Data.Semigroup Data.Monoid
+
 Functor
 
     ghci> fmap (+1) (MyJust 5)
     MyJust 6
 
     ghci> fmap (*2) MyNothing
+    MyNothing
+
+    ghci> 100 <$ MyJust 5
+    MyJust 100
+
+    ghci> 100 <$ MyNothing
     MyNothing
 
 Applicative
@@ -61,6 +75,15 @@ Applicative
 
     ghci> MyNothing <*> MyJust 10
     MyNothing
+
+    ghci> liftA2 (*) (MyJust 4) (MyJust 5)
+    MyJust 20
+
+    ghci> pure 1 *> MyJust 5
+    MyJust 5
+
+    ghci> pure 100 <* MyJust 5
+    MyJust 100
 
 Semigroup/Monoid
 
@@ -76,6 +99,19 @@ Semigroup/Monoid
     ghci> MyNothing <> MyNothing
     yNothing
 
+    ghci> sconcat (MyJust (Sum 1) :| [MyJust (Sum 2), MyJust (Sum 3)])
+    MyJust (Sum {getSum = 6})
+    
+    ghci> stimes 3 (MyJust "ab")
+    MyJust "ababab"
+
+    ghci> mappend (MyJust (Sum 2)) (MyJust (Sum 3))
+    MyJust (Sum {getSum = 5})
+
+    ghci> mconcat [MyJust (Sum 1), MyJust (Sum 2), MyJust (Sum 3)]
+    MyJust (Sum {getSum = 6})
+
+
 Foldable
 
     ghci> foldr (*) 2 MyNothing
@@ -89,5 +125,11 @@ Foldable
 
     ghci> foldMap Sum (MyJust 10)
     Sum {getSum = 10}
+
+    ghci> fold (MyJust (Sum 3))
+    Sum {getSum = 3}
+
+    ghci> fold MyNothing
+    ()
 
 -}
