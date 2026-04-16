d7d98eb52adb999474b1ec03e7b1d5b72b2bace3
Lebedevich Vlfdislav <lebedevich.vladislav@yandex.ru>
2026-04-16 03:05:31 +0300

    added data MyEither with constructors of MyLeft e and MyRight e ; added instances Functor and Foldable ; added matching examples

---
+++ practice_02/part2/myParser/src/MyTypes/MyEither.hs
@@ -0,0 +1,64 @@
+{-
+
+1. Создайте и настройте stack проект myParser
+
+2. Создайте в src директорию MyTypes и создайте в нем три модуля со следующими реализациями:
+- тип MyEither, аналогичный стандартному Either. Сделайте этот тип представителем классов типов Foldable, Semigroup, Functor и Applicative (*)
+(*) для других необходимых классов типов можно использовать механизм deriving
+
+Добавьте в конец каждого файла многострочный комментарий: 
+для реализованного типа и определенных представителей классов типов напишите пример вызова соответствующих функций и результатов работы из ghci 
+(если класс типов не требуется определять по заданию, то пример вызова не нужен):
+- fold, foldMap, foldr
+- (<>), sconcat, stimes
+- mappend, mconcat
+- fmap, (<$)
+- pure, (<*>), liftA2, (*>), (<*)
+
+3. Импортируйте модули в Main.hs
+
+-}
+
+module MyTypes.MyEither  ( MyEither(MyLeft, MyRight) ) where
+
+data MyEither e a = MyLeft e | MyRight a deriving (Show, Eq, Read)
+
+instance Functor (MyEither e) where
+    fmap _ (MyLeft x) = MyLeft x
+    fmap f (MyRight x) = MyRight (f x)
+
+instance Foldable (MyEither e) where
+    foldr _ z (MyLeft _) = z
+    foldr f z (MyRight x) = f x z
+    foldMap _ (MyLeft _) = mempty
+    foldMap f (MyRight x) = f x
+
+{-
+
+Functor
+    ghci> fmap (*2) (MyRight 5)
+    MyRight 10
+
+    ghci> fmap (+4) (MyLeft "error")
+    MyLeft "error"
+
+    ghci> 100 <$ (MyRight 5)
+    MyRight 100
+
+    ghci> 100 <$ (MyLeft "error")
+    MyLeft "error"
+
+Foldable
+
+    ghci> foldr (+) 10 (MyRight 5)
+    15
+
+    ghci> foldr (+) 10 (MyLeft "error")
+    10
+
+    ghci> foldMap Sum (MyRight 5)
+    Sum {getSum = 5}
+
+    ghci> foldMap Sum (MyLeft "error")
+    Sum {getSum = 0}
+-}
