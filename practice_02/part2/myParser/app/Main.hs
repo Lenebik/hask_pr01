a3e635fa72b2ec2cf74d8246241d77b688c5ca6e
Lebedevich Vlfdislav <lebedevich.vladislav@yandex.ru>
2026-04-23 21:23:32 +0300

    putSrtLn for attoparsec added

--- practice_02/part2/myParser/app/Main.hs
+++ practice_02/part2/myParser/app/Main.hs
@@ -5,6 +5,8 @@ import MyTypes.MyMaybe()
 import MyTypes.MyEither()
 
 import qualified MyParsers.MyParser as My
+import qualified MyParsers.ParsecParser as Parsec
+import qualified MyParsers.AttoparsecParser as Atto
 
 main :: IO ()
 main = do
@@ -14,8 +16,8 @@ main = do
     putStrLn $ show (My.runParser My.plusOrMult "12*345dsf")
     putStrLn $ show (My.runParser My.plusOrMult "12+345dsf")
     putStrLn "Parsec:"
-    -- putStrLn $ show (runParser plusOrMultParsec "12*345dsf")
-    -- putStrLn $ show (runParser plusOrMultParsec "12+345dsf")
+    putStrLn $ show (Parsec.runParsecParser Parsec.plusOrMultParsec "12*345dsf")
+    putStrLn $ show (Parsec.runParsecParser Parsec.plusOrMultParsec "12+345dsf")
     putStrLn "Attoparsec:"
-    -- putStrLn $ show (runParser plusOrMultAttoparsec "12*345dsf")
-    -- putStrLn $ show (runParser plusOrMultAttoparsec "12+345dsf")
+    putStrLn $ show (Atto.runParser Atto.plusOrMultAttoparsec "12*345dsf")
+    putStrLn $ show (Atto.runParser Atto.plusOrMultAttoparsec "12+345dsf")
