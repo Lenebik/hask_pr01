8db23ce6cbe9d57fcb92a04e3d68d419d8c6fb1c
Lebedevich Vlfdislav <lebedevich.vladislav@yandex.ru>
2026-04-23 15:52:06 +0300

    added Main.hs

--- practice_02/part2/myParser/app/Main.hs
+++ practice_02/part2/myParser/app/Main.hs
@@ -1,6 +1,21 @@
 module Main (main) where
 
-import Lib
+import MyTypes.MyTree()
+import MyTypes.MyMaybe()
+import MyTypes.MyEither()
+
+import qualified MyParsers.MyParser as My
 
 main :: IO ()
-main = someFunc
+main = do
+
+    putStrLn "\nMyParser"
+    putStrLn "MyParser:"
+    putStrLn $ show (My.runParser My.plusOrMult "12*345dsf")
+    putStrLn $ show (My.runParser My.plusOrMult "12+345dsf")
+    putStrLn "Parsec:"
+    -- putStrLn $ show (runParser plusOrMultParsec "12*345dsf")
+    -- putStrLn $ show (runParser plusOrMultParsec "12+345dsf")
+    putStrLn "Attoparsec:"
+    -- putStrLn $ show (runParser plusOrMultAttoparsec "12*345dsf")
+    -- putStrLn $ show (runParser plusOrMultAttoparsec "12+345dsf")
