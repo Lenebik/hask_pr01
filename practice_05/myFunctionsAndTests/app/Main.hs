37d24234489b89bf0ea2109c855b30c86ed9563d
Lebedevich Vlfdislav <lebedevich.vladislav@yandex.ru>
2026-05-21 23:27:47 +0300

    added normMod :: Int -> Int -> Int as helper function for addMod ; added addMod :: Int -> Int -> Int -> Int ; added splitTokens :: String -> [String] used for splitting String into list of tokens ; added clean :: [String] -> [String] to get rid of empty tokens ; added reverseWords :: String -> String

--- practice_05/myFunctionsAndTests/app/Main.hs
+++ practice_05/myFunctionsAndTests/app/Main.hs
@@ -1,6 +1,8 @@
 module Main (main) where
 
-import Lib
+import Lib (addMod)
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn ("addMod 7 8 5 = " ++ show (addMod 7 8 5))
+    putStrLn ("addMod (-3) 1 5 = " ++ show (addMod (-3) 1 5))
--- practice_05/myFunctionsAndTests/src/Lib.hs
+++ practice_05/myFunctionsAndTests/src/Lib.hs
@@ -1,6 +1,32 @@
 module Lib
-    ( someFunc
+    ( addMod
+    , reverseWords
+    , splitTokens
+    , clean
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- Приведение числа к остатку по модулю m через повторное вычитание/сложение.
+normMod :: Int -> Int -> Int
+normMod x m
+    | x < 0     = normMod (x + m) m
+    | x < m     = x
+    | otherwise = normMod (x - m) m
+
+-- Сложение двух чисел по модулю m
+addMod :: Int -> Int -> Int -> Int
+addMod x y m = normMod (x + y) m
+
+-- Разбивает строку на чередующиеся блоки слов и пробелов
+splitTokens :: String -> [String]
+splitTokens [] = []
+splitTokens (c:cs) =
+    let isSpace = c == ' '
+        (token, rest) = span (\x -> (x == ' ') == isSpace) (c:cs)
+    in token : splitTokens rest
+
+-- Убирает пустые токены
+clean :: [String] -> [String]
+clean = filter (not . null)
+
+reverseWords :: String -> String
+reverseWords = concat . reverse . splitTokens
